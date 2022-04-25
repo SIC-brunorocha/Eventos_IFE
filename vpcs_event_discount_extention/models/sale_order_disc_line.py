db2c504b3d8ba08b9a288b124a86abd0c18c59b8
SIC-brunorocha <68641905+SIC-brunorocha@users.noreply.github.com>
2022-04-25 18:25:17 +0100

    testes

--- vpcs_event_discount_extention/models/sale_order_disc_line.py
+++ vpcs_event_discount_extention/models/sale_order_disc_line.py
@@ -1,18 +1,140 @@
 from odoo import api, fields, models, _
+from odoo.tools.misc import formatLang
+
 
 class SaleOrder(models.Model):
     _inherit = "sale.order"
 
     def _create_reward_line(self, program):
-        for rec in self.order_line:
-            rec.write({
-                'discount':program.discount_percentage
-                })
+        if program.discount_type == 'percentage' and not program.discount_max_amount:
+            reward_line=self._get_reward_line_values(program)
+        else:
+            self.write({'order_line': [(0, False, value) for value in self._get_reward_line_values(program)]})
+
+    def _create_new_no_code_promo_reward_lines(self):
+        '''Apply new programs that are applicable'''
+        self.ensure_one()
+        order = self
+        programs = order._get_applicable_no_code_promo_program()
+        programs = programs._keep_only_most_interesting_auto_applied_global_discount_program()
+        for program in programs:
+            # VFE REF in master _get_applicable_no_code_programs already filters programs
+            # why do we need to reapply this bunch of checks in _check_promo_code ????
+            # We should only apply a little part of the checks in _check_promo_code...
+            error_status = program._check_promo_code(order, False)
+            if not error_status.get('error'):
+                if program.promo_applicability == 'on_next_order':
+                    order.state != 'cancel' and order._create_reward_coupon(program)
+                elif program.discount_line_product_id.id not in self.order_line.mapped('product_id').ids:
+                    if program.discount_type == 'percentage':
+                        reward_line=self._get_reward_line_values(program)
+                    else:
+                        self.write({'order_line': [(0, False, value) for value in self._get_reward_line_values(program)]})
+                if program.discount_type != 'percentage':
+                    order.no_code_promo_program_ids |= program
+
+    def _get_reward_values_discount(self, program):
+        if program.discount_type == 'fixed_amount':
+            taxes = program.discount_line_product_id.taxes_id
+            if self.fiscal_position_id:
+                taxes = self.fiscal_position_id.map_tax(taxes)
+            return [{
+                'name': _("Discount: %s", program.name),
+                'product_id': program.discount_line_product_id.id,
+                'price_unit': - self._get_reward_values_discount_fixed_amount(program),
+                'product_uom_qty': 1.0,
+                'product_uom': program.discount_line_product_id.uom_id.id,
+                'is_reward_line': True,
+                'tax_id': [(4, tax.id, False) for tax in taxes],
+            }]
+        reward_dict = {}
+        lines = self._get_paid_order_lines()
+        amount_total = sum(self._get_base_order_lines(program).mapped('price_subtotal'))
+        if program.discount_apply_on == 'cheapest_product':
+            line = self._get_cheapest_line()
+            if line:
+                if program.discount_type == 'percentage' and not program.discount_max_amount:
+                    line.write({
+                        'discount':program.discount_percentage
+                        })
+                discount_line_amount = min(line.price_reduce * (program.discount_percentage / 100), amount_total)
+                if discount_line_amount:
+                    taxes = self.fiscal_position_id.map_tax(line.tax_id)
+
+                    reward_dict[line.tax_id] = {
+                        'name': _("Discount: %s", program.name),
+                        'product_id': program.discount_line_product_id.id,
+                        'price_unit': - discount_line_amount if discount_line_amount > 0 else 0,
+                        'product_uom_qty': 1.0,
+                        'product_uom': program.discount_line_product_id.uom_id.id,
+                        'is_reward_line': True,
+                        'tax_id': [(4, tax.id, False) for tax in taxes],
+                    }
+        elif program.discount_apply_on in ['specific_products', 'on_order']:
+            if program.discount_apply_on == 'specific_products':
+                # We should not exclude reward line that offer this product since we need to offer only the discount on the real paid product (regular product - free product)
+                free_product_lines = self.env['coupon.program'].search([('reward_type', '=', 'product'), ('reward_product_id', 'in', program.discount_specific_product_ids.ids)]).mapped('discount_line_product_id')
+                lines = lines.filtered(lambda x: x.product_id in (program.discount_specific_product_ids | free_product_lines))
+
+            # when processing lines we should not discount more than the order remaining total
+            currently_discounted_amount = 0
+            for line in lines:
+                if program.discount_type == 'percentage' and not program.discount_max_amount:
+                    line.write({
+                        'discount':program.discount_percentage
+                        })
+                discount_line_amount = min(self._get_reward_values_discount_percentage_per_line(program, line), amount_total - currently_discounted_amount)
+
+                if discount_line_amount:
+
+                    if line.tax_id in reward_dict:
+                        reward_dict[line.tax_id]['price_unit'] -= discount_line_amount
+                    else:
+                        taxes = self.fiscal_position_id.map_tax(line.tax_id)
+
+                        reward_dict[line.tax_id] = {
+                            'name': _(
+                                "Discount: %(program)s - On product with following taxes: %(taxes)s",
+                                program=program.name,
+                                taxes=", ".join(taxes.mapped('name')),
+                            ),
+                            'product_id': program.discount_line_product_id.id,
+                            'price_unit': - discount_line_amount if discount_line_amount > 0 else 0,
+                            'product_uom_qty': 1.0,
+                            'product_uom': program.discount_line_product_id.uom_id.id,
+                            'is_reward_line': True,
+                            'tax_id': [(4, tax.id, False) for tax in taxes],
+                        }
+                        currently_discounted_amount += discount_line_amount
+
+        # If there is a max amount for discount, we might have to limit some discount lines or completely remove some lines
+        max_amount = program._compute_program_amount('discount_max_amount', self.currency_id)
+        if max_amount > 0:
+            amount_already_given = 0
+            for val in list(reward_dict):
+                amount_to_discount = amount_already_given + reward_dict[val]["price_unit"]
+                if abs(amount_to_discount) > max_amount:
+                    reward_dict[val]["price_unit"] = - (max_amount - abs(amount_already_given))
+                    add_name = formatLang(self.env, max_amount, currency_obj=self.currency_id)
+                    reward_dict[val]["name"] += "( " + _("limited to ") + add_name + ")"
+                amount_already_given += reward_dict[val]["price_unit"]
+                if reward_dict[val]["price_unit"] == 0:
+                    del reward_dict[val]
+        return reward_dict.values()
+
 
 class SaleCouponApplyCode(models.TransientModel):
     _inherit = 'sale.coupon.apply.code'
 
     def apply_coupon(self, order, coupon_code):
         res=super(SaleCouponApplyCode, self).apply_coupon(order, coupon_code)
-        order.applied_coupon_ids=None
+        program = self.env['coupon.program'].search([('promo_code', '=', coupon_code)])
+        if program:
+            if program.discount_type == 'percentage' and not program.discount_max_amount:
+                order.code_promo_program_id=None
+        else:
+            coupon = self.env['coupon.coupon'].search([('code', '=', coupon_code)], limit=1)
+            if coupon:
+                if coupon.program_id.discount_type == 'percentage' and not coupon.program_id.discount_max_amount:
+                    order.applied_coupon_ids=None
         return res
